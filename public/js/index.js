cc12ffc409f67528eb2de01474f8666b9d60ccad
Charles Norman <arioch3d@gmail.com>
2026-02-10 16:00:44 -0500

    Modified Quote Express Part 3

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+const quotes = [
+{
+    quote: "All Hands, Abandon ship!",
+    author: "Captain Picard",
+},
+{
+    quote: "Doh!",
+    author: "Homer Simpson",
+},
+{   quote: "The internet is the first thing that humaniity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+    author: "Eric Schmidt",
+},
+{
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+}
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+}, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
