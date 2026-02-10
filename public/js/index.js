8855e81320fdd42f22a646d3e23135e3aa0715a3
Charles Norman <arioch3d@gmail.com>
2026-02-10 16:23:52 -0500

    Updated with ASync Function

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,55 @@
 "use strict"
 
-const elements = {
-    quote: document.getElementById("quote"),
-    author: document.getElementById("author"),
-};
-const quotes = [
-{
-    quote: "All Hands, Abandon ship!",
-    author: "Captain Picard",
-},
-{
-    quote: "Doh!",
-    author: "Homer Simpson",
-},
-{   quote: "The internet is the first thing that humaniity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-    author: "Eric Schmidt",
-},
-{
-    quote: "The only way to do great work is to love what you do.",
-    author: "Steve Jobs",
-}
-]
+async function getRandomImage(){
+    const clientId = "MY_API_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random?client_id=${clientId}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const recievedPhotoUrl = returnedData.urls.regular;
 
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${recievedPhotoUrl}")`;
+} catch (error) {
+    console.error(error)
+}
+}
+
+getRandomImage();
+
+/* const elements = {
+     quote: document.getElementById("quote"),
+     author: document.getElementById("author"),
+ };
+ const quotes = [
+ {
+     quote: "All Hands, Abandon ship!",
+     author: "Captain Picard",
+ },
+ {
+     quote: "Doh!",
+     author: "Homer Simpson",
+ },
+ {   quote: "The internet is the first thing that humaniity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+     author: "Eric Schmidt",
+ },
+ {
+     quote: "The only way to do great work is to love what you do.",
+     author: "Steve Jobs",
+ }
+ ]
+
+ function loopThroughQuotes() {
+     let quoteIndex = 0;
+     setInterval(() => {
+         if (quoteIndex < quotes.length) {
+             elements.quote.textContent = quotes[quoteIndex].quote;
+             elements.author.textContent = quotes[quoteIndex].author;
             quoteIndex++;
         } else {
-            quoteIndex = 0;
-        }
-}, 3000);
-}
+             quoteIndex = 0;
+         }
+ }, 3000);
+ }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
